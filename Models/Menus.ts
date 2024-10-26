e6a1b7243900c7dc6a3b1ff7d518aede9c5231d9
pradip jedhe <pradipjedhe69@gmail.com>
2024-10-26 22:21:16 +0530

    add to the Menu Schema

---
+++ Models/Menus.ts
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+interface menu {
+    restaurantId: mongoose.Schema.Types.ObjectId[],
+    name: string,
+    description: string,
+    price: number,
+    menuPictuer: string
+}
+const MenuSchema: mongoose.Schema<menu> = new mongoose.Schema({
+    restaurantId: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Restaurant",
+        required: true,
+    }],
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    menuPictuer: {
+        type: String,
+        required: true,
+    }
+})
+
+export default mongoose.model("meun", MenuSchema)
