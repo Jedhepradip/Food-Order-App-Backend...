f9d080490f6825bb3736e9b16db420ad432d4018
pradip jedhe <pradipjedhe69@gmail.com>
2024-10-26 21:40:17 +0530

    Restaurant Updated get all info

--- Controllers/RestaurantControllers.ts
+++ Controllers/RestaurantControllers.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express"
 import Restaurant from "../Models/Restaurant";
+import UserModels from "../Models/UserModels";
 
 interface CustomRequest extends Request {
     user?: {
@@ -49,6 +50,54 @@ export const GetRestaurantData = async (req: CustomRequest, res: Response): Prom
     try {
         const userId = req.user?.id
         const existingRestaurant = await Restaurant.findOne({ user: userId });
+        if (!existingRestaurant) {
+            return res.status(400).json({ message: "Restaurant Not Created..." })
+        }
+        return res.status(200).json(existingRestaurant)
+    } catch (error) {
+        console.log(error);
+        return res.status(501).json({ message: "Internal Server Error..." })
+    }
+}
+
+// Get All Restaurant Data
+export const GetAllRestaurantData = async (req: Request, res: Response): Promise<any> => {
+    try {
+        const RestaurantData = await Restaurant.find()
+        if (!RestaurantData) {
+            return res.status(400).json({ message: "Restaurant Not Fount...." })
+        }
+        return res.status(200).json(RestaurantData)
+    } catch (error) {
+        console.log(error);
+        return res.status(501).json({ message: "Internal Server Error..." })
+    }
+}
+
+// Restaurant Updated Admin Or User 
+export const RestaurantUpdate = async (req: Request, res: Response): Promise<any> => {
+    try {
+        const RestaurantId = req.params?.id
+        const { restaurantName, city, country, deliveryTime, cuisines } = req.body;
+        const RestaurantReq = { restaurantName, city, country, deliveryTime, cuisines }
+        const RestaurantFind = await Restaurant.findById(RestaurantId)
+        if (!RestaurantFind) {
+            return res.status(400).json({ message: "Restaurant Not Found..." })
+        }
+
+        // if(!req.files){
+        //     RestaurantFind.imageUrl = req.files
+        // }
+
+        if (!restaurantName) RestaurantReq.restaurantName = RestaurantFind.restaurantName
+        if (!deliveryTime) RestaurantReq.deliveryTime = RestaurantFind.deliveryTime
+        if (!cuisines) RestaurantReq.cuisines = RestaurantFind.cuisines
+        if (!country) RestaurantReq.country = RestaurantFind.country
+        if (!city) RestaurantReq.city = RestaurantFind.city
+
+        const RestaurantData = await Restaurant.findByIdAndUpdate(RestaurantId, RestaurantReq, { new: true });
+        return res.status(200).json({ message: "Restaurant Updated Successfully...", RestaurantData })
+
     } catch (error) {
         console.log(error);
         return res.status(501).json({ message: "Internal Server Error..." })
--- Router/RestaurantRouter.ts
+++ Router/RestaurantRouter.ts
@@ -1,8 +1,11 @@
 import express from "express"
 const router = express.Router()
-import { RestaurantCreate } from "../Controllers/RestaurantControllers"
+import { RestaurantCreate, GetRestaurantData, GetAllRestaurantData, RestaurantUpdate } from "../Controllers/RestaurantControllers"
 import { jwtMiddleware } from "../Middewares/jwtAuthMiddleware"
 
 router.post("/Create/Restaurant/User", jwtMiddleware, RestaurantCreate)
+router.get("/Get/Restaurant/Data", jwtMiddleware, GetRestaurantData)
+router.get("/Get/RestaurantData/AllUser", jwtMiddleware, GetAllRestaurantData)
+router.put("/Restaurant/Updated/:id", jwtMiddleware, RestaurantUpdate)
 
 export default router
